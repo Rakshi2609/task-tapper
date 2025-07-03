ee5e653478ac28d7e3e3f624a715bf66c50afffd
Rakshi2609 <rakshithganjimut@gmail.com>
2025-07-03 19:37:35 +0530

    Update store.js

--- frontend/src/assests/store.js
+++ frontend/src/assests/store.js
@@ -4,8 +4,8 @@ import { persist } from "zustand/middleware";
 
 const API_BASE_URL = import.meta.env.VITE_APP_API_URL;
 const API_URL = import.meta.env.MODE === "development"
-  ? "http://localhost:5000/api/function"
-  : `${API_BASE_URL}/api/function`;
+  ? "http://localhost:5000/api/auth"
+  : `${API_BASE_URL}/api/auth`;
 
 axios.defaults.withCredentials = true;
 
